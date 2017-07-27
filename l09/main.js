68e9215d92fe17e17dd72de92342428637b52756
thienth32 <thienth32@gmail.com>
2017-07-27 18:17:20 +0700

    update lesson 10

--- l09/main.js
+++ l09/main.js
@@ -1,107 +1,60 @@
-/*var name = 13;
-
-console.log(name);
-
-name = "dep trai";
-console.log(name);
 
 
-var name = prompt("Ho va ten:");
-var address = prompt("Dia chi:");
+/**
+ * Hàm
+ */
+function tinhTong(a, b, c){
+	return a+b+c;
+}
 
-document.write(name + " - " + address);
+/**
+ * biến cục bộ
+ */
+function bienCucBo(){
+	var a = 5;
 
-*/
+}
 
-/*var a = prompt("Canh A:");
-var b = prompt("Canh B:");
-var c = prompt("Canh C:");
+// bienCucBo();
+// console.log(a);
+var x = 9;
+if(x > 0){
+	var y = 6;
+}else{
+	var y = 0;
+}
 
-a = parseInt(a);
-b = parseInt(b);
-c = parseInt(c);
-debugger;
-document.write(a + b + c);
+for (var i = 0; i < 100; i++) {
+	var z = i;
+}
+// console.log(z);
+switch(z){
+	case 99:
+		var w = 5;
+		break;
+	default:
+		break;
+}
 
-// boolean
-var boo = true; // false
-// null
-var nul = null;
-// string
-var nul = "";
-// undefined
-// console.log(thienth);
-// number
-var num = 6;
-var num2 = 6.5;
-// object
-var obj = {
-	name: "thienth",
-	age: 26,
-	hometown: 'nam dinh',
-	arr: []
-};
-console.log(obj);
+// console.log(w);
 
-obj.name = 't3h';
-console.log(obj);
+/**
+ * biến toàn cục
+ */
+function bienToanCuc(){
+	console.log(z);
+}
 
-// array
-var arr = [1,3,5,'someone', [99, 100], {age: 19, school: 't3h'}];
-console.log(arr);
-*/
-// cau lenh re nhanh
-// 
-// var num = prompt("dien cai gi vao day:");
-// if(num > 6){
-// 	console.log('so nay lon hon 6');
-// }else if(num == 6){
-// 	console.log('so nay bang 6');
-// }else{
-// 	console.log('so nay nho hon 6');
-// }
+bienToanCuc();
 
-
-// var test = num > 6 
-// 		? "test se lon hon 6" : "test nho hon hoac bang 6";
-
-// console.log(test);
-// Math.sqrt()
-
-// var a = parseInt(prompt("so A: "));
-// var b = parseInt(prompt("so B: "));
-// var c = parseInt(prompt("so C: "));
-
-// if(a+b > c && a+c > b && b+c>a){
-// 	var p = a+b+c;
-// 	var p2 = p/2;
-// 	var s = Math.sqrt(p2*(p2-a)*(p2-b)*(p2-c));
-// 	debugger;
-// 	document.write(`
-// 		<h2>Do dai 3 canh (${a}, ${b}, ${c}) co hinh thanh dc tam giac</h2>
-// 		<p>Chu vi: <b>${p}</b></p>
-// 		<p>Dien tich: <b>${s}</b></p>
-// 	`);
-// }else{
-// 	document.write(`<h1>Do dai 3 canh (${a}, ${b}, ${c}) khong hinh thanh tam giac</h1>`)
-// }
-// 
-var arr = [1,2,3,4,5,6,7,8,9];
-console.log(arr.length);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/**
+ * Xử lý sự 
+ */
+var count = 0;
+function CountEvent(){
+	count++;
+	console.log(count);
+}
 
 
 
