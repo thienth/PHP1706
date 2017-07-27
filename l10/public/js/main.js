4cdce33a4ecdb5c8fa342e6a9304be7529655710
thienth32 <thienth32@gmail.com>
2017-07-27 20:25:04 +0700

    update draw playground

--- l10/public/js/main.js
+++ l10/public/js/main.js
@@ -7,8 +7,16 @@ var imgArr = [
 var currentSlide = 0;
 var targetImg = 
 		document.getElementById('main-image');
+function zoomIn(){
+	targetImg.width = 600;
+}
+function zoomOut(){
+	targetImg.width = 400;
+}
+
 function previous(){
-	
+	currentSlide = currentSlide == 0 ? imgArr.length-1 : currentSlide-1;
+	targetImg.src = imgArr[currentSlide];
 }
 function next(){
 	currentSlide = 
@@ -16,3 +24,19 @@ function next(){
 							? 0 : currentSlide+1;
 	targetImg.src = imgArr[currentSlide];
 }
+
+setInterval(function(){
+	next();
+}, 3000);
+
+
+
+
+
+
+
+
+
+
+
+
