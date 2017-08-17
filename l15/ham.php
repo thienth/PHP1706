521d56857cd2cd9283800b8e8e21e095ff8acbb0
thienth32 <thienth32@gmail.com>
2017-08-17 19:10:31 +0700

    update

---
+++ l15/ham.php
@@ -0,0 +1,32 @@
+<?php 
+function test(){
+	echo "minh thich thi minh viet dong 1<br>";
+	echo "minh thich thi minh viet dong 2<br>";
+	echo "minh thich thi minh viet dong 3<br>";
+	echo "minh thich thi minh viet dong =4<br>";
+	echo "minh thich thi minh viet dong 5<br>";
+}
+
+
+test();
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+test();
+// 
+// 
+// 
+// 
+// 
+test();
+
+
+
+ ?>
