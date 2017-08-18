0327b8a94794059b989de909c424363ebb9b52eb
thienth32 <thienth32@gmail.com>
2017-08-18 09:56:36 +0700

    update

--- l15/ham.php
+++ l15/ham.php
@@ -1,31 +1,12 @@
 <?php 
-function test(){
-	echo "minh thich thi minh viet dong 1<br>";
-	echo "minh thich thi minh viet dong 2<br>";
-	echo "minh thich thi minh viet dong 3<br>";
-	echo "minh thich thi minh viet dong =4<br>";
-	echo "minh thich thi minh viet dong 5<br>";
+//required parameter
+//optional parameter
+function tinhtong($a, $b, $c = 0){
+	echo $a+$b+$c;
 }
 
 
-test();
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-test();
-// 
-// 
-// 
-// 
-// 
-test();
+tinhtong(4,5);
 
 
 
