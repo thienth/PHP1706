de633c2c8064d333c65f83d7c57da578a0fbe603
HoangHuan <hoanghuan021@gmail.com>
2017-10-01 19:29:30 +0700

    update

--- l25-mvc/views/admin/category/add.php
+++
@@ -1,5 +0,0 @@
-<form action="index.php?r=admin/category/save-add" method="post" >
-	
-	<input type="text" name="title" value="" placeholder="Post title">
-	<button type="submit">Submit</button>
-</form>
